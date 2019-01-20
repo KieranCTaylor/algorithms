2b1b9e5372818df89e4dd4721c3081699089ca3a
kieran <KieranT94@gmail.com>
2019-01-20 22:11:21 +0000

    Add two sum description

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,9 @@
+"""
+Given a list of integers in input file, find if there exists two distinct elements that sum to make each integer
+between -10,000 to 10,000. Return the count of how many integers in the range for which this is true.
+(this solution is very simple but as a result also very slow)
+"""
+
 problem_path = 'problem_files/2Sum.txt'
 
 with open(problem_path) as data_file:
